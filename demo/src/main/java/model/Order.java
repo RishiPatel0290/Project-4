f8d28c4d560905f880407bb3b0df0ab2ea056aa3
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2022-11-15 22:51:46 -0500

    classes added to model package

---
+++ demo/src/main/java/model/Order.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Order implements Customizable{
+    @Override
+    public boolean add(Object obj) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object obj) {
+        return false;
+    }
+}
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
-
-
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+    exports com.example.controller;
+    opens com.example.controller to javafx.fxml;
 }
