a474c7c4ca4b4594ecb9fefc7827350c9e5a5bce
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2022-11-17 15:11:52 -0500

    updates

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
     exports com.example.controller;
     opens com.example.controller to javafx.fxml;
 }
